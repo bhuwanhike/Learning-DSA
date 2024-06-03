a7bac4aff43cd84e1726b94b2d1411cadcf3714d
Bhuwan-17 <bhuwansharmajee@gmail.com>
2024-06-03 21:31:37 +0530

    7.#functions.cpp

---
+++ 7.#functions.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+using namespace std;
+
+// Note: uncomment the code you want to use.
+
+
+// Power of a number:
+
+// int power(int a, int b){
+//     int ans = 1;
+//     for (int i=1; i<=b; i++){
+//         ans*=a;
+//     }
+//     return ans;
+// }
+
+//__________________*_____________________*__________________
+
+
+// nCr:
+
+// int factorial(int n){
+//     int i, fact=1;
+//     for (i=1; i<=n; i++){
+//         fact = i*fact;
+//     }
+//     return fact;
+// }
+
+// int nCr(int n, int r){
+//     int num = factorial(n);
+//     int den = factorial(r)*factorial(n-r);
+//     int result = num/den;
+//     return result;
+// }
+
+//___________________*________________*_____________________
+
+
+// Prime number:
+
+// bool isPrime(int n){
+//     for(int i = 2; i<n; i++){
+//         if(n%i==0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+//___________________*_________________*__________________
+
+
+
+// nth term of AP:
+
+// int AP(int n){
+//     int ap= (3*n) + 7;
+//     return ap;
+// }
+
+//_______________*_________________*_____________________
+
+
+
+// Fibonacci series (nth term):
+
+// int fibbo(int n){
+//     int a = 0, b = 1;
+//     int temp;
+
+//     if (n == 1){
+//         return a;
+//     }
+//     else if (n == 2){
+//         return b;
+//     }
+//     else {
+//         temp = a;
+//         a = b;
+//         b = fibbo(n-1)+fibbo(n-2);
+//         return b;
+//     }
+// }
+
+//___________________*___________________*___________________
+
+
+int main(){
+// Note: uncomment the code you want to use.
+
+
+// cout<<power(2,3);
+
+//cout<<(nCr(13,0));
+
+//cout<<factorial(0);
+
+//cout<<isPrime(89);
+
+//cout<<AP(3);
+
+//cout<<fibbo(10);
+
+
+}
+
+
+
+
+
