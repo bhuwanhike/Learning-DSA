018f67c76e654b2cd26b92dff418df45e9edda18
Bhuwan Sharma <69743169+bhuwanhike@users.noreply.github.com>
2024-09-08 18:27:17 +0530

    Update 11.#Two_Sum.cpp

--- 11.#Two_Sum.cpp
+++ 11.#Two_Sum.cpp
@@ -2,26 +2,23 @@
 using namespace std;
 // Brute force
 
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         vector<int> p;
-//         for(int i = 0; i<nums.size(); i++){
-//             for(int k = i+1; k<nums.size(); k++){
-//                 if((nums[k]+nums[i])== target){
-//                     p.push_back(k);
-//                     p.push_back(i);
-//                 }
-//             }
-//         }   
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> p;
+        for(int i = 0; i<nums.size(); i++){
+            for(int k = i+1; k<nums.size(); k++){
+                if((nums[k]+nums[i])== target){
+                    p.push_back(k);
+                    p.push_back(i);
+                }
+            }
+        }   
         
-//         return p;
-//     }
-// };
+        return p;
+    }
+};
 
 
 // Optimal approach
-int main(){
-    map<int,int> p;
-    cout<<p[0];
-}
+
